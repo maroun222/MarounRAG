18cc208782aeca52010e4fc2d599258a5926929a
unknown <maroun.hajj@net.usj.edu.lb>
2026-07-22 13:20:41 +0300

    feat: initialize shadcn UI components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
+import App from "./App.jsx"
+import "./index.css"
+
+import { TooltipProvider } from "@/components/ui/tooltip"
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
   </StrictMode>,
-);
+)
