4b2cf85239704f2499466a230ea5402fbafd3681
unknown <maroun.hajj@net.usj.edu.lb>
2026-07-23 13:59:15 +0300

    feat: configure secure RAG API options

--- middleware/RagMiddleware.Api/Program.cs
+++ middleware/RagMiddleware.Api/Program.cs
@@ -1,22 +1,28 @@
+using RagMiddleware.Infrastructure.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+    .AddOptions<RagApiOptions>()
+    .Bind(
+        builder.Configuration.GetSection(
+            RagApiOptions.SectionName
+        )
+    )
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
